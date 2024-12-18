f8de0396e88fa09265461314d5666660bfadb5c7
Karan (Nat) <discordantreality@protonmail.com>
2024-12-18 00:40:25 +0000

    Added .gitignore and made changes to entrypoint

--- usr/bin/entrypoint.sh
+++ usr/bin/entrypoint.sh
@@ -11,9 +11,3 @@ TARGET_FILE="/etc/systemd/system/feedbin-init.service"
 
 # Replace the placeholder ${TS_HOSTNAME}.${TS_TAILNAME} in the target file
 sed -i "s/\${TS_HOSTNAME}\.\${TS_TAILNAME}/$TS_HOSTNAME.$TS_TAILNAME/g" "$TARGET_FILE"
-
-# Optionally, print the modified content for verification
-# cat "$TARGET_FILE"
-
-# Start the main process (replace with your container's main process)
-exec "$@"
